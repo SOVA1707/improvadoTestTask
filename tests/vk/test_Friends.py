e33c1e8ab46e1bfd952735886c7ec5d41053e63e
SOVA1707 <81626778+SOVA1707@users.noreply.github.com>
2022-04-25 11:34:14 +0700

    add new get_friends_info tests

--- tests/vk/test_Friends.py
+++ tests/vk/test_Friends.py
@@ -7,8 +7,15 @@ user_id = 0
 
 class MyTestCase(unittest.TestCase):
     def test_get_friends_info(self):
-        user1 = User(user_id, token)
-        self.assertEqual(type([]), type(user1.get_friends_info()))
+        user1 = User(token, user_id)
+        user1_friend_info = user1.get_friends_info()
+        self.assertEqual(type([]), type(user1_friend_info))
+        self.assertTrue('first_name' in user1_friend_info[0])
+        self.assertTrue('last_name' in user1_friend_info[0])
+        self.assertTrue('country' in user1_friend_info[0])
+        self.assertTrue('city' in user1_friend_info[0])
+        self.assertTrue('bdate' in user1_friend_info[0])
+        self.assertTrue('sex' in user1_friend_info[0])
 
 
 if __name__ == '__main__':
